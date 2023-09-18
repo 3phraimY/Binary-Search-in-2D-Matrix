3a2a895cf3b5ac2d17f44e9a3760bf6ef7adfb90
3phraimY <ephraim.young@cuw.edu>
2023-09-18 14:46:30 -0500

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,60 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-		System.out.println(MatrixSearch(arr,5)[0]);
-		System.out.println(MatrixSearch(arr,5)[1]);
-		
-		
-		
-	}
-	public static int[] MatrixSearch(int[][] arr, int key){
-		int totalLength = arr[1].length * arr.length -1;
-		int currentMax = totalLength;
-		int currentMin = 0;
-		int[] index = new int[] {-1,-1};
-		int currentPos = (currentMax + currentMin)/2;
-		
-		
-		if(key > arr[2][3] || key < arr[0][0])
-		{
-			return index;
-		}
-		//run until gap between max and min is 1
-		while(true)
-		{
-			//if match found
-			if(arr[currentPos/3][currentPos%4] == key)
-			{
-				//set index values
-				index[0] = currentPos/3;
-				index[1] = currentPos%4;
-				return index;
-			}
-			//no match found
-			else
-			{
-				//if current middle is bigger than searching for
-				if(arr[currentPos/3][currentPos %4 ] > key)
-				{
-					currentMax = currentPos;
-				}
-				//if current middle is smaller than searching for
-				else
-				{
-					currentMin = currentPos;
-				}
-			}
-			//change middle new middle
-			currentPos = (currentMax+currentMin)/2;
-			if(currentMax - currentMin == 1)
-			{
-				break;
-			}
-		}
-		return index;
-	}
-	
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println(MatrixSearch(arr, 12)[0] + "," + MatrixSearch(arr, 12)[1]);
+    }
 
+    // Binary search within ascending int[][]
+    // Returns int[] with position in matrix
+    
+    public static int[] MatrixSearch(int[][] arr, int key)
+    {
+        // Calculate the total number of elements in the matrix
+        int totalLength = (arr[1].length * arr.length) -1;
+        // Initialize variables for binary search
+        int currentMax = totalLength;
+        int currentMin = 0;
+        int[] index = new int[] {-1,-1};
+        int currentPos = 0;
+        // Get the number of rows and columns in the matrix
+        int numRows = arr.length;
+        int numCols = arr[0].length;
+
+        // Check if the key is outside the bounds of the matrix
+        if (key > arr[numRows-1][numCols-1] || key < arr[0][0]) 
+        {
+            return index;
+        }
+
+        // Run binary search
+        while (currentMax >= currentMin) 
+        {
+        	//set center
+            currentPos = currentMin + (currentMax - currentMin) / 2;
+            
+            // If match found, set index values and return
+            if (arr[currentPos / numCols][currentPos % numCols] == key) 
+            {
+                index[0] = currentPos / numCols;
+                index[1] = currentPos % numCols;
+                return index;
+            } 
+            //if larger than center
+            else if (arr[currentPos / numCols][currentPos % numCols] < key) 
+            {
+            	//move min up to center
+                currentMin = currentPos + 1;
+            } 
+            //if smaller than center
+            else 
+            {
+            	//move max down to center
+                currentMax = currentPos - 1;
+            }
+        }
+
+        // Return default index values if key is not found
+        return index;
+    }
 }
